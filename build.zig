0701090f72ea1e29c0f73fc55bd1eb5683964588
hz <47288761+xxhertz@users.noreply.github.com>
2025-01-31 16:49:58 -0500

    fix: incorrect source filename

--- build.zig
+++ build.zig
@@ -2,6 +2,6 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     _ = b.addModule("hooking", .{
-        .root_source_file = b.path("src/trampoline.zig"),
+        .root_source_file = b.path("src/hooking.zig"),
     });
 }
