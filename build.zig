82b65ff7fc5b70367c50a908a7ca7cba60bc18e2
hz <47288761+xxhertz@users.noreply.github.com>
2025-02-01 07:45:22 -0500

    fix: include zigwin32 in buildstep

--- build.zig
+++ build.zig
@@ -1,7 +1,9 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    _ = b.addModule("hooking", .{
+    const module = b.addModule("hooking", .{
         .root_source_file = b.path("src/hooking.zig"),
     });
+    const zigwin32_dep = b.dependency("zigwin32", .{});
+    module.addImport("zigwin32", zigwin32_dep.module("zigwin32"));
 }
